7dfd2ad65b5073157b21c01043b3888d76d27cbb
Artem Kondyukov <artemkondyukov@gmail.com>
2016-07-10 00:02:14 +0300

    Refactoring

--- calculatortest.py
+++ calculatortest.py
@@ -1,34 +1,30 @@
 import random
 import unittest
 
-from solver import *
+from calculator import evaluate_infix
 
 
-class SolverTest(unittest.TestCase):
+class CalculatorTest(unittest.TestCase):
     def testCase_1(self):
-        solver = Solver()
         input_string = "(3+(4-1))*5"
-        exp_output = "30"
-        self.assertEqual(solver.evaluate_infix(input_string), exp_output)
+        exp_output = None, None, "30"
+        self.assertEqual(evaluate_infix(input_string), exp_output)
 
     def testCase_2(self):
-        solver = Solver()
         input_string = "2 * x + 0.5 = 1"
-        exp_output = "x = 0.25"
-        self.assertEqual(solver.evaluate_infix(input_string), exp_output)
+        exp_output = None, None, "x = 0.25"
+        self.assertEqual(evaluate_infix(input_string), exp_output)
 
     def testCase_3(self):
-        solver = Solver()
         input_string = "2x + 1 = 2(1-x)"
-        exp_output = "x = 0.25"
-        self.assertEqual(solver.evaluate_infix(input_string), exp_output)
+        exp_output = None, None, "x = 0.25"
+        self.assertEqual(evaluate_infix(input_string), exp_output)
 
     def testCase_4(self):
-        solver = Solver()
         strlen = random.randint(0, 30)
         possible_chars = list("1234567890-+=*/.,()logn")
         for i in range(100000):
             random_string = ''.join(random.choice(possible_chars) for _ in range(strlen))
             print(random_string)
-            solver.evaluate_infix(random_string)
+            evaluate_infix(random_string)
         self.assertTrue(True)
