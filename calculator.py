6ccddc8d5f4f213ca835453655fc7ba82a1fc483
Artem Kondyukov <artemkondyukov@gmail.com>
2016-07-10 02:16:13 +0300

    Refactoring

--- calculator.py
+++ calculator.py
@@ -11,29 +11,36 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        err_start, err_end, message = evaluate_infix(request.form['expression'])
-        expression = request.form['expression']
+        err_start, err_end, message, expression = evaluate_infix(request.form['expression'])
         if err_start is None:
             return render_template('index.html', solution=message, expression=expression)
         else:
-            return render_template('index.html',
-                                   explanation=message,
-                                   before_mistake=expression[:err_start],
-                                   mistake=expression[err_start:err_end+1],
-                                   after_mistake=expression[err_end+1:])
+            if err_start != -1:
+                return render_template('index.html',
+                                       explanation=message,
+                                       before_mistake=expression[:err_start],
+                                       mistake=expression[err_start:err_end + 1],
+                                       after_mistake=expression[err_end + 1:])
+            else:
+                return render_template('index.html',
+                                       explanation=message,
+                                       before_mistake=expression,
+                                       mistake="",
+                                       after_mistake="")
 
 
-def evaluate_infix(expression=None):
+def evaluate_infix(expression):
     """
     The highest level function for evaluation of expressions
     :param expression: string representing an infix expression
     :return: err_start, err_end, message in case of any error, None, None, solution otherwise
     """
+    print(expression)
     postfix_expression = PostfixExpression()
     if "=" in expression:  # Solve an equation
         if expression.count("=") > 1:
-            pos = expression[expression.index("=")+1:].index("=")
-            return pos, pos, "More than one '=' symbols in expression can't be interpreted"
+            pos = expression.index("=") + expression[expression.index("=") + 1:].index("=") + 1
+            return pos, pos, "More than one '=' symbols in expression can't be interpreted", expression
         else:
             left_part = expression.split("=")[0]
             right_part = expression.split("=")[1]
@@ -42,7 +49,7 @@ def evaluate_infix(expression=None):
                 right_side_operand = postfix_expression.process_infix_string(right_part)
             except ValueError as e:
                 place = postfix_expression.error_place
-                return place[0], place[1], e.args[0]
+                return place[0], place[1], e.args[0], expression
 
         polynomial = (left_side_operand - right_side_operand).polynomial
     else:
@@ -50,26 +57,26 @@ def evaluate_infix(expression=None):
             polynomial = postfix_expression.process_infix_string(expression).polynomial
         except ValueError as e:
             place = postfix_expression.error_place
-            return place[0], place[1], e.args[0]
+            return place[0], place[1], e.args[0], postfix_expression.interpreted_expression
 
     solver = PolynomialSolver()
     try:
         # Take the first (and the only) solution
         result = solver.solve(polynomial)[0]
     except NotImplementedError as e:
-        return -1, -1, e.args[0]
+        return -1, -1, e.args[0], expression
     if result[1] == 0:
         if '=' in expression:
             return -1, -1, "The expression doesn't have a variable " + \
-                           "(or the coefficient before it is 0), but " + \
-                           "has a '=' sign. It cannot be interpreted."
+                   "(or the coefficient before it is 0), but " + \
+                   "has a '=' sign. It cannot be interpreted.", expression
         # Formatting the result
         num_result = result[0] if math.floor(result[0]) != result[0] else math.floor(result[0])
-        return None, None, str(num_result)
+        return None, None, str(num_result), expression
     else:
         if '=' not in expression:
             return -1, -1, "The expression has variables but doesn't have '=' sign." + \
-                           "Should it be treated as equation?"
+                   "Should it be treated as equation?", expression
         # Formatting the result
         num_result = result[0] if math.floor(result[0]) != result[0] else result[0]
-        return None, None, postfix_expression.varname + ' = ' + str(num_result)
+        return None, None, postfix_expression.varname + ' = ' + str(num_result), expression
--- calculatortest.py
+++ calculatortest.py
@@ -7,18 +7,18 @@ from calculator import evaluate_infix
 class CalculatorTest(unittest.TestCase):
     def testCase_1(self):
         input_string = "(3+(4-1))*5"
-        exp_output = None, None, "30"
-        self.assertEqual(evaluate_infix(input_string), exp_output)
+        exp_output = "30"
+        self.assertEqual(evaluate_infix(input_string)[2], exp_output)
 
     def testCase_2(self):
         input_string = "2 * x + 0.5 = 1"
-        exp_output = None, None, "x = 0.25"
-        self.assertEqual(evaluate_infix(input_string), exp_output)
+        exp_output = "x = 0.25"
+        self.assertEqual(evaluate_infix(input_string)[2], exp_output)
 
     def testCase_3(self):
         input_string = "2x + 1 = 2(1-x)"
-        exp_output = None, None, "x = 0.25"
-        self.assertEqual(evaluate_infix(input_string), exp_output)
+        exp_output = "x = 0.25"
+        self.assertEqual(evaluate_infix(input_string)[2], exp_output)
 
     def testCase_4(self):
         strlen = random.randint(0, 30)
